e7431e60656aadefe37f151feb463f916effa250
Simon Ordish <simon.ordish@masagi.co.uk>
2026-06-03 14:28:46 +0200

    prpilot 0.2.0

--- Casks/prpilot.rb
+++ Casks/prpilot.rb
@@ -1,6 +1,6 @@
 cask "prpilot" do
-  version "0.1.3"
-  sha256 "9832e01aceffdaee9da722e099583f4d0c10b2490d0bf77f16aca92310d9128d"
+  version "0.2.0"
+  sha256 "6b186a1cf2ed2a338acb91f0edd910b2053cc5b94f1dc38d593bbbc8c06531e4"
 
   url "https://github.com/ordishs/PRPilot/releases/download/v#{version}/PRPilot-#{version}.dmg"
   name "PR Pilot"
